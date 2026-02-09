b30cdcb3c0be7b4590d22381c22d175e74eba42f
remabd <remiabdallah3@gmail.com>
2026-02-09 11:07:16 +0100

    feat: script analyse

---
+++ scripts/analyse.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+echo "Bonjour, vous avez rentré " $# " paramètres";
+echo "Le nom du script est: " $0;
+echo "Le troisième paramètre est " $3;
+echo "Voici la liste des paramètres: " $@;
