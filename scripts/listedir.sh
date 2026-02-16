87b681fa68f7aa99327f99eed97fcd73123a00f5
remabd <remiabdallah3@gmail.com>
2026-02-16 11:04:25 +0100

    feat: script liste des dossiers

--- scripts/listedir.sh
+++ scripts/listedir.sh
@@ -3,10 +3,24 @@ if [ $# != 1 ]; then
     echo "test-fichier.sh: expected 1 argument; got" "$#"
 else
     if [ -d $1 ]; then
-        RESULTD = ""
-        RESULTF = ""
-        for i in [ ]
+        RESULTD=()
+        RESULTF=()
+        for filename in $1*; do
+            if [ -d ${filename} ]; then
+                RESULTD+=("${filename}")
+            else
+                RESULTF+=("${filename}")
+            fi
+        done
 
+        echo "###### Fichiers dans $1"
+        for f in "${RESULTF[@]}"; do
+            echo "${f}"
+        done
+        echo "###### Répertoires dans $1"
+        for f in "${RESULTD[@]}"; do
+            echo "${f}"
+        done
     else
         echo "test-fichier.sh: $1 is not a directory"
     fi
